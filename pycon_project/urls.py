9c36f7e042c218b3634bbda8f25cea331d67db98
Jannis Leidel <jannis@leidel.info>
2010-06-28 22:38:18 +0200

    Added urls, settings and views.

--- pycon_project/urls.py
+++ pycon_project/urls.py
@@ -5,6 +5,9 @@ from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 admin.autodiscover()
 
+from pinax.apps.account.openid_consumer import PinaxConsumer
+
+#from pycon_project.views import creole_preview
 
 
 handler500 = "pinax.views.server_error"
@@ -15,8 +18,20 @@ urlpatterns = patterns("",
     url(r"^$", direct_to_template, {
         "template": "homepage.html",
     }, name="home"),
+
+    url(r"^account/signup/$", "pinax.apps.account.views.signup", name="acct_signup"),
+    url(r"^account/", include("pinax.apps.account.urls")),
+    url(r"^openid/(.*)", PinaxConsumer()),
+    url(r"^oauth_access/finish_signup/(?P<service>\w+)/$", "oauth_callbacks.finish_signup", name="oauth_access_finish_signup"),
+    url(r"^oauth_access/", include("oauth_access.urls")),
+    url(r"^waitinglist/", include("pinax.apps.waitinglist.urls")),
+    url(r"^admin/", include(admin.site.urls)),
     
-    (r"^admin/", include(admin.site.urls)),
+   # url(r"^creole_preview/$", creole_preview, name="creole_preview"),
+    
+    url(r"^feed/(?P<section>\w+)/$", "biblion.views.blog_feed", name="blog_feed"),
+
+    (r"^", include("wakawaka.urls")),
 )
 
 
