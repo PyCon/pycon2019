fd77a0625b8da93be1ed3f2b1617c31a69ec7307
Brian Rosner <brosner@gmail.com>
2010-08-19 15:19:40 -0600

    merged in Alex's changes to sponsors

--- pycon_project/apps/sponsors/models.py
+++ pycon_project/apps/sponsors/models.py
@@ -1,7 +1,10 @@
 import datetime
 
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
+from django.contrib.auth.models import User
 
 
 class SponsorLevel(models.Model):
@@ -19,17 +22,29 @@ class SponsorLevel(models.Model):
 
 class Sponsor(models.Model):
     
+    applicant = models.OneToOneField(
+        User, related_name="sponsorship", verbose_name=_("applicant")
+    )
     name = models.CharField(_("name"), max_length=100)
     external_url = models.URLField(_("external URL"))
     annotation = models.TextField(_("annotation"), blank=True)
     contact_name = models.CharField(_("contact name"), max_length=100)
     contact_email = models.EmailField(_(u"Contact e\u2011mail"))
-    level = models.ForeignKey(SponsorLevel, verbose_name=_("level"))
+    level = models.ForeignKey(SponsorLevel, verbose_name=_("level"), null=True)
     added = models.DateTimeField(_("added"), default=datetime.datetime.now)
-    active = models.BooleanField(_("active"), default=False)
+    active = models.NullBooleanField(_("active"), choices=[
+        (True, "approved"),
+        (False, "rejected"),
+        (None, "unreviewed"),
+    ])
     
     def __unicode__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        if self.active:
+            return reverse("sponsor_detail", kwargs={"pk": self.pk})
+        return reverse("sponsor_index")
     
     @property
     def website_logo_url(self):
