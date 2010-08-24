9e96acb840bdc792112878f60b3102653e1888f3
Carl Meyer <carl@oddbird.net>
2010-08-24 13:28:35 -0400

    fix choices display on Sponsor.active

--- pycon_project/apps/sponsors/admin.py
+++ pycon_project/apps/sponsors/admin.py
@@ -8,9 +8,16 @@ class SponsorLogoInline(admin.StackedInline):
     extra = 1
 
 
+class SponsorAdmin(admin.ModelAdmin):
+    def get_form(self, *args, **kwargs):
+        # @@@ kinda ugly but using choices= on NullBooleanField is broken
+        form = super(SponsorAdmin, self).get_form(*args, **kwargs)
+        form.base_fields['active'].widget.choices = [(u'1', "unreviewed"),
+                                                     (u'2', "approved"),
+                                                     (u'3', "rejected")]
+        return form
+    
+    inlines = [SponsorLogoInline]
+    
 admin.site.register(SponsorLevel)
-admin.site.register(Sponsor,
-    inlines = [
-        SponsorLogoInline
-    ]
-)
+admin.site.register(Sponsor, SponsorAdmin)
