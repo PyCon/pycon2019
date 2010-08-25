3d344c3ea01ef12af0bd8ff2e5c0c8a662d01cf5
Carl Meyer <carl@oddbird.net>
2010-08-24 23:34:16 -0400

    models and admin for variable benefits

--- pycon_project/apps/sponsors/models.py
+++ pycon_project/apps/sponsors/models.py
@@ -1,7 +1,9 @@
 import datetime
 
+from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.db.models.signals import post_init, post_save
 from django.utils.translation import ugettext_lazy as _
 
 from django.contrib.auth.models import User
@@ -11,6 +13,7 @@ class SponsorLevel(models.Model):
     
     name = models.CharField(_("name"), max_length=100)
     order = models.IntegerField(_("order"), default=0)
+    cost = models.PositiveIntegerField(_("cost"))
     description = models.TextField(_("description"), blank=True, help_text=_("This is private."))
     
     class Meta:
@@ -41,25 +44,123 @@ class Sponsor(models.Model):
         if self.active:
             return reverse("sponsor_detail", kwargs={"pk": self.pk})
         return reverse("sponsor_index")
-    
-    @property
-    def website_logo_url(self):
+
+    def reset_benefits(self):
+        """
+        Reset all benefits for this sponsor to the defaults for their
+        sponsorship level.
+
+        """
+        level = None
         try:
-            logo = SponsorLogo.objects.get(sponsor=self, label="website")
-        except SponsorLogo.DoesNotExist:
-            return u""
-        else:
-            return logo.logo.url
+            level = self.level
+        except SponsorLevel.DoesNotExist:
+            pass
+
+        allowed_benefits = []
+        if level:
+            for benefit_level in level.benefit_levels.all():
+                # Create all needed benefits if they don't exist already
+                sponsor_benefit, created = SponsorBenefit.objects.get_or_create(
+                    sponsor=self, benefit=benefit_level.benefit)
+            
+                # and set to default limits for this level.
+                sponsor_benefit.max_words = benefit_level.max_words
+                sponsor_benefit.other_limits = benefit_level.other_limits
+
+                # and set to active
+                sponsor_benefit.active
+
+                # @@@ We don't call sponsor_benefit.clean here. This means
+                # that if the sponsorship level for a sponsor is adjusted
+                # downwards, an existing too-long text entry can remain,
+                # and won't raise a validation error until it's next
+                # edited.
+                sponsor_benefit.save()
+
+                allowed_benefits.append(sponsor_benefit.pk)
+
+        # Any remaining sponsor benefits that don't normally belong to
+        # this level are set to inactive
+        self.sponsor_benefits.exclude(pk__in=allowed_benefits).update(
+            active=False, max_words=None, other_limits='')
+
+def _store_initial_level(sender, instance, **kwargs):
+    if instance:
+        instance._initial_level_id = instance.level_id
+
+post_init.connect(_store_initial_level, sender=Sponsor)
+
+def _check_level_change(sender, instance, created, **kwargs):
+    if instance and (created or
+                     instance.level_id != instance._initial_level_id):
+        instance.reset_benefits()
+
+post_save.connect(_check_level_change, sender=Sponsor)
 
 
-class SponsorLogo(models.Model):
+class Benefit(models.Model):
     
-    sponsor = models.ForeignKey(Sponsor, verbose_name=_("sponsor"))
-    label = models.CharField(
-        max_length = 100,
-        help_text = _("To display this logo on site use label 'website'")
-    )
-    logo = models.ImageField(_("logo"), upload_to="sponsor_logos/")
+    name = models.CharField(_("name"), max_length=100)
+    description = models.TextField(_("description"), blank=True)
+    type = models.CharField(_("type"),
+                            choices=[('text', 'Text'),
+                                     ('file', 'File'),
+                                     ('simple', 'Simple')],
+                            max_length=10,
+                            default='simple')
+
+    def __unicode__(self):
+        return self.name
+
+
+class BenefitLevel(models.Model):
     
+    benefit = models.ForeignKey(Benefit,
+                                related_name="benefit_levels",
+                                verbose_name=_("benefit"))
+    level = models.ForeignKey(SponsorLevel,
+                              related_name="benefit_levels",
+                              verbose_name=_("level"))
+    
+    max_words = models.PositiveIntegerField(_("max words"), blank=True, null=True)
+    other_limits = models.CharField(_("other limits"), max_length=200, blank=True)
+
     class Meta:
-        unique_together = [("sponsor", "label")]
+        ordering = ["level"]
+    
+    def __unicode__(self):
+        return u"%s - %s" % (self.level, self.benefit)
+
+
+class SponsorBenefit(models.Model):
+    
+    sponsor = models.ForeignKey(Sponsor,
+                                related_name="sponsor_benefits",
+                                verbose_name=_("sponsor"))
+    benefit = models.ForeignKey(Benefit,
+                                related_name="sponsor_benefits",
+                                verbose_name=_("benefit"))
+
+    active = models.BooleanField(default=True)
+    
+    # Limits: will initially be set to defaults from corresponding BenefitLevel
+    max_words = models.PositiveIntegerField(_("max words"), blank=True, null=True)
+    other_limits = models.CharField(_("other limits"), max_length=200, blank=True)
+
+    # Data: zero or one of these fields will be used, depending on the
+    # type of the Benefit (text, file, or simple)
+    text = models.TextField(_("text"), blank=True)
+    upload = models.FileField(_("file"), blank=True,
+                              upload_to='sponsor_files')
+
+    class Meta:
+        ordering = ['-active']
+    
+    def __unicode__(self):
+        return u"%s - %s" % (self.sponsor, self.benefit)
+    
+    def clean(self):
+        if self.max_words and len(self.text.split()) > self.max_words:
+            raise ValidationError('Sponsorship level only allows for %s words.'
+                                  % max_words)
