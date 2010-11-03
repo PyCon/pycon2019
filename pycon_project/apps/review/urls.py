a1b07dd553cfc05574c38065eed3feac88c6dd73
Alex Gaynor <alex.gaynor@gmail.com>
2010-11-02 21:20:17 -0400

    Added an opt out button.

--- pycon_project/apps/review/urls.py
+++ pycon_project/apps/review/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns("review.views",
     url(r"^stats/$", "review_stats", name="review_stats"),
     url(r"^(?P<pk>\d+)/$", "review_detail", name="review_detail"),
     url(r"^assignments/$", "review_assignments", name="review_assignments"),
+    url(r"^assignment/(?P<pk>\d+)/opt-out/$", "review_assignment_opt_out",
+        name="review_assignment_opt_out"),
 )
--- pycon_project/apps/review/views.py
+++ pycon_project/apps/review/views.py
@@ -197,3 +197,14 @@ def review_assignments(request):
     return render_to_response("reviews/review_assignment.html", {
         "assignments": assignments,
     }, context_instance=RequestContext(request))
+
+@login_required
+@require_POST
+def review_assignment_opt_out(request, pk):
+    review_assignment = get_object_or_404(ReviewAssignment,
+        pk=pk,
+        user=request.user
+    )
+    review_assignment.opted_out = True
+    review_assignment.save()
+    return redirect("review_assignments")
