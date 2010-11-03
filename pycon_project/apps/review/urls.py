8f46b19c5754e7bef53bd6f825e34cb7aca969bd
Brian Rosner <brosner@gmail.com>
2010-11-03 17:20:11 -0600

    broke proposal stats lists into their own pages

--- pycon_project/apps/review/urls.py
+++ pycon_project/apps/review/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import patterns, url, include, handler404, handler500
 
+
 urlpatterns = patterns("review.views",
     url(r"^list/$", "review_list", name="review_list"),
     url(r"^list/(?P<username>[\w]+)/$", "review_list", name="review_list_user"),
     url(r"^admin/$", "review_admin", name="review_admin"),
     url(r"^stats/$", "review_stats", name="review_stats"),
+    url(r"^stats/(?P<key>[\w]+)/$", "review_stats", name="review_stats_key"),
     url(r"^(?P<pk>\d+)/$", "review_detail", name="review_detail"),
 )
