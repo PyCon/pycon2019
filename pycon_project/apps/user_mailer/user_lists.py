8111c74e1f7435860f0bedbaa30ff278bdbe1258
Brian Rosner <brosner@gmail.com>
2010-12-22 15:30:55 -0700

    fixed whitespace

--- pycon_project/apps/user_mailer/user_lists.py
+++ pycon_project/apps/user_mailer/user_lists.py
@@ -2,8 +2,10 @@ from django.contrib.auth.models import Group, User
 
 from schedule.models import Session
 
+
 # @@@ Would probably be a good idea to consolidate the
 # accepted speaker functions
+
 
 def accepted_speakers():
     speakers = set()
@@ -12,6 +14,7 @@ def accepted_speakers():
             if speaker is not None and speaker.user is not None:
                 speakers.add(speaker.user)
     return iter(speakers)
+
 
 def accepted_talk_speakers():
     speakers = set()
@@ -23,27 +26,32 @@ def accepted_talk_speakers():
                 speakers.add(speaker.user)
     return iter(speakers)
 
+
 def accepted_panel_speakers():
     speakers = set()
     panels = Session.objects.filter(session_type=Session.SESSION_TYPE_PANEL)
-
+    
     for session in panels.select_related("speaker__user"):
         for speaker in session.speakers():
             if speaker is not None and speaker.user is not None:
                 speakers.add(speaker.user)
     return iter(speakers)
 
+
 def organizers():
     for user in User.objects.filter(is_staff=True):
         yield user
+
 
 def reviewers():
     for user in Group.objects.get(name="reviewers").user_set.all():
         yield user
 
+
 def reviewers_tutorial():
     for user in Group.objects.get(name="reviewers-tutorial").user_set.all():
         yield user
+
 
 # @@@ move to settings.py and accept dotted paths
 user_lists = [
