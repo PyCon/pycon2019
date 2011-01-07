9e661755a0661853eb43e6631da3024a3164b5d1
Glenn Siegman <gsiegman@gsiegman.com>
2011-01-06 23:15:28 -0500

    promote proposals management command for bulk promotion

---
+++ pycon_project/apps/review/management/commands/promoteproposals.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from review.models import ProposalResult, promote_proposal
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        accepted_proposals = ProposalResult.objects.filter(accepted=True)
+        
+        for result in accepted_proposals:
+            promote_proposal(result.proposal)
+        
