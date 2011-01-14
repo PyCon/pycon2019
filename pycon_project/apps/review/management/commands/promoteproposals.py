ab6cbd00ee0f84a029a50e8cec0c7104bc77cbe3
Brian Rosner <brosner@gmail.com>
2011-01-13 21:00:20 -0700

    ensure Session objects are given the same PK as their proposal so we don't break URLs

--- pycon_project/apps/review/management/commands/promoteproposals.py
+++ pycon_project/apps/review/management/commands/promoteproposals.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.db import connections
 
 from review.models import ProposalResult, promote_proposal
 
@@ -7,6 +8,8 @@ class Command(BaseCommand):
     
     def handle(self, *args, **options):
         accepted_proposals = ProposalResult.objects.filter(accepted=True)
+        accepted_proposals = accepted_proposals.order_by("proposal")
         
         for result in accepted_proposals:
             promote_proposal(result.proposal)
+        connections["default"].cursor().execute("SELECT setval('schedule_session_id_seq', (SELECT max(id) FROM schedule_session))")
