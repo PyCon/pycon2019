141f0c36430367d501ff089cf2c7d78f9fb8a3a2
Brian Rosner <brosner@gmail.com>
2011-02-08 01:38:43 -0700

    added SessionRole to admin

--- pycon_project/apps/schedule/admin.py
+++ pycon_project/apps/schedule/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from schedule.models import Presentation, Slot, Session, Track
+from schedule.models import SessionRole, Presentation, Slot, Session, Track
 
 
 admin.site.register(Session)
+admin.site.register(SessionRole,
+    list_display = ["session", "user", "role", "status", "submitted"],
+    raw_id_fields = ["user"],
+)
 
 admin.site.register(Track,
     list_display = ["pk", "name"]
