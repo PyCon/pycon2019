bcd9c099ee3e7d64c4601057751d191f61ac843a
Brian Rosner <brosner@gmail.com>
2011-02-27 14:20:17 -0700

    added caching on schedule

---
+++ pycon_project/apps/schedule/templatetags/schedule_tags.py
@@ -0,0 +1,47 @@
+from __future__ import with_statement
+
+from django import template
+
+import redis
+
+
+db = redis.Redis(host="127.0.0.1")
+try:
+    db.ping()
+except redis.ConnectionError:
+    db = None
+
+register = template.Library()
+
+
+class ScheduleCacheNode(template.Node):
+    
+    @classmethod
+    def handle_token(cls, parser, token):
+        nodelist = parser.parse(("endschedule_cache",))
+        parser.delete_first_token()
+        return cls(nodelist)
+    
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+    
+    def render(self, context):
+        if db:
+            key = "pycon2011-schedule"
+            output = db.get(key)
+            if output is None:
+                with db.lock("%s-lock" % key):
+                    # check for cached data if we lost lock acquisition and
+                    # if nothing was returned we can be sure we own the lock
+                    output = db.get(key)
+                    if output is None:
+                        output = self.nodelist.render(context)
+                        db.set(key, output)
+        else:
+            output = self.nodelist.render(context)
+        return output
+
+
+@register.tag
+def schedule_cache(parser, token):
+    return ScheduleCacheNode.handle_token(parser, token)
