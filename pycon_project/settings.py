1f24d2396bc89111688c28e86446026c7e58e2ed
Brian Rosner <brosner@gmail.com>
2011-06-28 20:27:53 -0600

    removed PYCON_YEAR from spots not actually needed

--- pycon_project/settings.py
+++ pycon_project/settings.py
@@ -58,20 +58,20 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "site_media", "media", PYCON_YEAR)
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "site_media", "media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = "/%s/site_media/media/%s/" % (PYCON_YEAR, PYCON_YEAR)
+MEDIA_URL = "/%s/site_media/media/" % PYCON_YEAR
 
 # Absolute path to the directory that holds static files like app media.
 # Example: "/home/media/media.lawrence.com/apps/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "site_media", "static", PYCON_YEAR)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "site_media", "static")
 
 # URL that handles the static files like app media.
 # Example: "http://media.lawrence.com"
-STATIC_URL = "/%s/site_media/static/%s/" % (PYCON_YEAR, PYCON_YEAR)
+STATIC_URL = "/%s/site_media/static/" % PYCON_YEAR
 
 # Additional directories which hold static files
 STATICFILES_DIRS = [
