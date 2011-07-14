aa1ea545317ac72f0490830504dfcc280ad4b83e
Patrick Altman <paltman@gmail.com>
2011-07-14 16:48:06 -0500

    added mark it up widget to popup

--- pycon_project/apps/boxes/forms.py
+++ pycon_project/apps/boxes/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 
+from markitup.widgets import MarkItUpWidget
+
 from boxes.models import Box
 
 
 class BoxForm(forms.ModelForm):
+    
+    content = forms.CharField(widget=MarkItUpWidget())
     
     class Meta:
         model = Box
