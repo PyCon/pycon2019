05b687846ee44816d9de0695a38eb165fc4c3a9f
Yulka plekhanova <yulka@insatsu.us>
2011-07-31 23:28:28 +0200

    added static template for sponsors about page

--- pycon_project/urls.py
+++ pycon_project/urls.py
@@ -47,9 +47,12 @@ urlpatterns = patterns("",
         url(r"^venue/bars/", direct_to_template, {"template": "venue/bars.html"}, name="bars"),
         url(r"^venue/shopping/", direct_to_template, {"template": "venue/shopping.html"}, name="shopping"),
         url(r"^venue/share-room/", direct_to_template, {"template": "venue/share_room.html"}, name="share_room"),
+        url(r"^sponsors/about/$", direct_to_template, {"template": "sponsors/about.html"}, name="about"),
+
         url(r"^boxes/", include("boxes.urls")),
-        
+
         url(r"^", include("wakawaka.urls")),
+
     ))),
 )
 
