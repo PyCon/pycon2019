3a1e6359b12b9f0e25f621af17b558cab928f369
Luke Hatcher <lukeman@gmail.com>
2011-08-14 17:22:46 -0400

    category back to a foreign key

--- pycon_project/migrations/010_add_presentation_category.sql
+++ pycon_project/migrations/010_add_presentation_category.sql
@@ -5,11 +5,5 @@ CREATE TABLE "conference_presentationcategory" (
 );
 CREATE INDEX "conference_presentationcategory_slug" ON "conference_presentationcategory" ("slug");
 CREATE INDEX "conference_presentationcategory_slug_like" ON "conference_presentationcategory" ("slug" varchar_pattern_ops);
-
-
-CREATE TABLE "proposals_proposal_categories" (
-    "id" serial NOT NULL PRIMARY KEY,
-    "proposal_id" integer NOT NULL REFERENCES "proposals_proposal" ("id") DEFERRABLE INITIALLY DEFERRED,
-    "presentationcategory_id" integer NOT NULL REFERENCES "conference_presentationcategory" ("id") DEFERRABLE INITIALLY DEFERRED,
-    UNIQUE ("proposal_id", "presentationcategory_id")
-);
+ 
+ALTER TABLE "proposals_proposal" ADD COLUMN "category_id" integer NOT NULL REFERENCES "conference_presentationcategory" ("id") DEFERRABLE INITIALLY DEFERRED;
