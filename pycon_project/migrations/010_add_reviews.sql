367d518ad68d51a22a604c58d84f9b9e66a3dbf9
Luke Hatcher <lukeman@gmail.com>
2011-09-14 03:13:56 -0400

    add reviews migration

---
+++ pycon_project/migrations/010_add_reviews.sql
@@ -0,0 +1,71 @@
+BEGIN;
+CREATE TABLE "review_reviewassignment" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "proposal_id" integer NOT NULL REFERENCES "proposals_proposal" ("id") DEFERRABLE INITIALLY DEFERRED,
+    "user_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED,
+    "origin" integer NOT NULL,
+    "assigned_at" timestamp with time zone NOT NULL,
+    "opted_out" boolean NOT NULL
+)
+;
+CREATE TABLE "review_proposalmessage" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "proposal_id" integer NOT NULL REFERENCES "proposals_proposal" ("id") DEFERRABLE INITIALLY DEFERRED,
+    "user_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED,
+    "message" text NOT NULL,
+    "message_html" text NOT NULL,
+    "submitted_at" timestamp with time zone NOT NULL
+)
+;
+CREATE TABLE "review_review" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "proposal_id" integer NOT NULL REFERENCES "proposals_proposal" ("id") DEFERRABLE INITIALLY DEFERRED,
+    "user_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED,
+    "vote" varchar(2) NOT NULL,
+    "comment" text NOT NULL,
+    "comment_html" text NOT NULL,
+    "submitted_at" timestamp with time zone NOT NULL
+)
+;
+CREATE TABLE "review_latestvote" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "proposal_id" integer NOT NULL REFERENCES "proposals_proposal" ("id") DEFERRABLE INITIALLY DEFERRED,
+    "user_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED,
+    "vote" varchar(2) NOT NULL,
+    "submitted_at" timestamp with time zone NOT NULL,
+    UNIQUE ("proposal_id", "user_id")
+)
+;
+CREATE TABLE "review_proposalresult" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "proposal_id" integer NOT NULL UNIQUE REFERENCES "proposals_proposal" ("id") DEFERRABLE INITIALLY DEFERRED,
+    "score" numeric(5, 2) NOT NULL,
+    "comment_count" integer CHECK ("comment_count" >= 0) NOT NULL,
+    "vote_count" integer CHECK ("vote_count" >= 0) NOT NULL,
+    "plus_one" integer CHECK ("plus_one" >= 0) NOT NULL,
+    "plus_zero" integer CHECK ("plus_zero" >= 0) NOT NULL,
+    "minus_zero" integer CHECK ("minus_zero" >= 0) NOT NULL,
+    "minus_one" integer CHECK ("minus_one" >= 0) NOT NULL,
+    "accepted" boolean
+)
+;
+CREATE TABLE "review_comment" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "proposal_id" integer NOT NULL REFERENCES "proposals_proposal" ("id") DEFERRABLE INITIALLY DEFERRED,
+    "commenter_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED,
+    "text" text NOT NULL,
+    "public" boolean NOT NULL,
+    "commented_at" timestamp with time zone NOT NULL
+)
+;
+CREATE INDEX "review_reviewassignment_proposal_id" ON "review_reviewassignment" ("proposal_id");
+CREATE INDEX "review_reviewassignment_user_id" ON "review_reviewassignment" ("user_id");
+CREATE INDEX "review_proposalmessage_proposal_id" ON "review_proposalmessage" ("proposal_id");
+CREATE INDEX "review_proposalmessage_user_id" ON "review_proposalmessage" ("user_id");
+CREATE INDEX "review_review_proposal_id" ON "review_review" ("proposal_id");
+CREATE INDEX "review_review_user_id" ON "review_review" ("user_id");
+CREATE INDEX "review_latestvote_proposal_id" ON "review_latestvote" ("proposal_id");
+CREATE INDEX "review_latestvote_user_id" ON "review_latestvote" ("user_id");
+CREATE INDEX "review_comment_proposal_id" ON "review_comment" ("proposal_id");
+CREATE INDEX "review_comment_commenter_id" ON "review_comment" ("commenter_id");
+COMMIT;
