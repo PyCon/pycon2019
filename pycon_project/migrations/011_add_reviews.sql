ba4f0bc24bdd5c3a3050dbfc06f1df37c2b36e09
Luke Hatcher <lukeman@gmail.com>
2011-09-29 05:23:48 -0400

    change to markitup for review fields

--- pycon_project/migrations/011_add_reviews.sql
+++ pycon_project/migrations/011_add_reviews.sql
@@ -1,4 +1,4 @@
-BEGIN;
+### New Model: review.ReviewAssignment
 CREATE TABLE "review_reviewassignment" (
     "id" serial NOT NULL PRIMARY KEY,
     "proposal_id" integer NOT NULL REFERENCES "proposals_proposal" ("id") DEFERRABLE INITIALLY DEFERRED,
@@ -8,25 +8,28 @@ CREATE TABLE "review_reviewassignment" (
     "opted_out" boolean NOT NULL
 )
 ;
+### New Model: review.ProposalMessage
 CREATE TABLE "review_proposalmessage" (
     "id" serial NOT NULL PRIMARY KEY,
     "proposal_id" integer NOT NULL REFERENCES "proposals_proposal" ("id") DEFERRABLE INITIALLY DEFERRED,
     "user_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED,
     "message" text NOT NULL,
-    "message_html" text NOT NULL,
-    "submitted_at" timestamp with time zone NOT NULL
+    "submitted_at" timestamp with time zone NOT NULL,
+    "_message_rendered" text NOT NULL
 )
 ;
+### New Model: review.Review
 CREATE TABLE "review_review" (
     "id" serial NOT NULL PRIMARY KEY,
     "proposal_id" integer NOT NULL REFERENCES "proposals_proposal" ("id") DEFERRABLE INITIALLY DEFERRED,
     "user_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED,
     "vote" varchar(2) NOT NULL,
     "comment" text NOT NULL,
-    "comment_html" text NOT NULL,
-    "submitted_at" timestamp with time zone NOT NULL
+    "submitted_at" timestamp with time zone NOT NULL,
+    "_comment_rendered" text NOT NULL
 )
 ;
+### New Model: review.LatestVote
 CREATE TABLE "review_latestvote" (
     "id" serial NOT NULL PRIMARY KEY,
     "proposal_id" integer NOT NULL REFERENCES "proposals_proposal" ("id") DEFERRABLE INITIALLY DEFERRED,
@@ -36,6 +39,7 @@ CREATE TABLE "review_latestvote" (
     UNIQUE ("proposal_id", "user_id")
 )
 ;
+### New Model: review.ProposalResult
 CREATE TABLE "review_proposalresult" (
     "id" serial NOT NULL PRIMARY KEY,
     "proposal_id" integer NOT NULL UNIQUE REFERENCES "proposals_proposal" ("id") DEFERRABLE INITIALLY DEFERRED,
@@ -49,13 +53,15 @@ CREATE TABLE "review_proposalresult" (
     "accepted" boolean
 )
 ;
+### New Model: review.Comment
 CREATE TABLE "review_comment" (
     "id" serial NOT NULL PRIMARY KEY,
     "proposal_id" integer NOT NULL REFERENCES "proposals_proposal" ("id") DEFERRABLE INITIALLY DEFERRED,
     "commenter_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED,
     "text" text NOT NULL,
     "public" boolean NOT NULL,
-    "commented_at" timestamp with time zone NOT NULL
+    "commented_at" timestamp with time zone NOT NULL,
+    "_text_rendered" text NOT NULL
 )
 ;
 CREATE INDEX "review_reviewassignment_proposal_id" ON "review_reviewassignment" ("proposal_id");
@@ -68,4 +74,3 @@ CREATE INDEX "review_latestvote_proposal_id" ON "review_latestvote" ("proposal_i
 CREATE INDEX "review_latestvote_user_id" ON "review_latestvote" ("user_id");
 CREATE INDEX "review_comment_proposal_id" ON "review_comment" ("proposal_id");
 CREATE INDEX "review_comment_commenter_id" ON "review_comment" ("commenter_id");
-COMMIT;
