1f288857e3e632e1c2a26d6274ea65898ad48e62
Luke Hatcher <lukeman@gmail.com>
2011-11-17 12:52:33 -0500

    require logged in user for registration

--- pycon_project/apps/registration/views.py
+++ pycon_project/apps/registration/views.py
@@ -3,9 +3,12 @@ import hashlib
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
+from django.contrib.auth.decorators import login_required
+
 from constance import config
 
 
+@login_required
 def cte_login(request):
     
     salt = config.CTE_SECRET
