f504cdbcee66d660f3b462bfa67b0734068e4afd
Luke Hatcher <lukeman@gmail.com>
2011-11-17 12:50:03 -0500

    restrict box editing to staff

--- pycon_project/apps/boxes/views.py
+++ pycon_project/apps/boxes/views.py
@@ -1,28 +1,17 @@
-from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
-from boxes.authorization import load_can_edit
+from django.contrib.admin.views.decorators import staff_member_required
+
 from boxes.forms import BoxForm
 from boxes.models import Box
 
 
-# @@@ problem with this is that the box_edit.html and box_create.html won't have domain objects in context
-def get_auth_vars(request):
-    auth_vars = {}
-    if request.method == "POST":
-        keys = [k for k in request.POST.keys() if k.startswith("boxes_auth_")]
-        for key in keys:
-            auth_vars[key.replace("boxes_auth_", "")] = request.POST.get(key)
-        auth_vars["user"] = request.user
-    return auth_vars
-
-
+@staff_member_required
 def box_edit(request, pk):
+
     box = get_object_or_404(Box, pk=pk)
     if request.method == "POST":
-        #if not load_can_edit()(request, **get_auth_vars(request)):
-        #    return HttpResponseForbidden()
         
         form = BoxForm(request.POST, instance=box)
         if form.is_valid():
@@ -38,10 +27,10 @@ def box_edit(request, pk):
     return render_to_response("boxes/box_edit.html", ctx)
 
 
+@staff_member_required
 def box_create(request, label):
+
     if request.method == "POST":
-        #if not load_can_edit()(request, **get_auth_vars(request)):
-        #    return HttpResponseForbidden()
         
         form = BoxForm(request.POST)
         if form.is_valid():
