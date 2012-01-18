8c501fd59c4c6900d07adbcbd91c15ed18ec583f
Luke Hatcher <lukeman@gmail.com>
2012-01-18 18:07:35 -0500

    temporarily disabling the json schedule (gaynor'd)

--- pycon_project/apps/symposion/schedule/urls.py
+++ pycon_project/apps/symposion/schedule/urls.py
@@ -22,5 +22,5 @@ urlpatterns = patterns("symposion.schedule.views",
     url(r"^session/(\d+)/$", "session_detail", name="schedule_session_detail"),
     url(r"^tutorials/", "schedule_tutorials", name="schedule_tutorials"),
     url(r"^bookmarks/(\d+)/(\w+)/$", "schedule_user_bookmarks", name="schedule_user_bookmarks"),
-    url(r"^json/$", "schedule_json", name="schedule_json"),
+    #url(r"^json/$", "schedule_json", name="schedule_json"),
 )
