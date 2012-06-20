b6b1ccfcbfc9003c40aa76d944a78dd331d309a9
James Tauber <jtauber@jtauber.com>
2012-06-20 19:44:10 -0400

    fixed PROJECT_ROOT

--- symposion/settings.py
+++ symposion/settings.py
@@ -4,7 +4,7 @@
 import os.path
 import posixpath
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
