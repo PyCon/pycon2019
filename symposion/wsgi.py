c010dcd724ca65ab161f4beb7f2778e6674a7b85
James Tauber <jtauber@jtauber.com>
2012-06-20 19:33:12 -0400

    revert to default as we have to set via env:set anyway for manage.py

--- symposion/wsgi.py
+++ symposion/wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "symposion.settings_gondor")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "symposion.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
