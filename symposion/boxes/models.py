7e64ec240fe28e97cd7a43ee07beb7ab7ae09f1b
Luke Hatcher <lukeman@gmail.com>
2012-06-21 18:51:10 -0400

    boxes can be blank

--- symposion/boxes/models.py
+++ symposion/boxes/models.py
@@ -10,7 +10,7 @@ from markitup.fields import MarkupField
 class Box(models.Model):
     
     label = models.CharField(max_length=100, db_index=True)
-    content = MarkupField()
+    content = MarkupField(blank=True)
     
     created_by = models.ForeignKey(User, related_name="boxes")
     last_updated_by = models.ForeignKey(User, related_name="updated_boxes")
