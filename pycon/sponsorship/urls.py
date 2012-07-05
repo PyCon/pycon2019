d537084dfebaff07b9e7c556dda7e38ea284c54f
James Tauber <jtauber@jtauber.com>
2012-07-05 16:30:11 -0400

    removed unused sponsorship info page

--- pycon/sponsorship/urls.py
+++ pycon/sponsorship/urls.py
@@ -4,7 +4,6 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns("pycon.sponsorship.views",
     url(r"^$", direct_to_template, {"template": "sponsorship/list.html"}, name="sponsor_list"),
-    url(r"^info/$", "sponsor_info", name="sponsor_info"),
     # url(r"^jobs/$", direct_to_template, {"template": "sponsors/jobs.html"}, name="sponsor_jobs"),
     url(r"^apply/$", "sponsor_apply", name="sponsor_apply"),
     url(r"^terms/$", direct_to_template, {"template": "sponsors/terms.html"}, name="sponsor_terms"),
