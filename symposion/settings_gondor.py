1e2809c212cb9d185381f47f8eb04090addb37a1
Brian Rosner <brosner@gmail.com>
2012-07-05 15:53:14 -0600

    removed MEDIA_URL and STATIC_URL from settings_gondor.py

--- symposion/settings_gondor.py
+++ symposion/settings_gondor.py
@@ -27,8 +27,6 @@ SITE_ID = 1 # set this to match your Sites setup
 MEDIA_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"], "site_media", "media")
 STATIC_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"], "site_media", "static")
 
-MEDIA_URL = "/site_media/media/" # make sure this maps inside of site_media_url
-STATIC_URL = "/site_media/static/" # make sure this maps inside of site_media_url
 ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
 
 FILE_UPLOAD_PERMISSIONS = 0640
