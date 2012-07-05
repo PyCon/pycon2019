d812c6c972f6a3cc32cb4d5d55e27d53c00ad0ab
James Tauber <jtauber@jtauber.com>
2012-07-05 16:30:40 -0400

    removed unused imports

--- pycon/sponsorship/urls.py
+++ pycon/sponsorship/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url, include, handler404, handler500
+from django.conf.urls.defaults import patterns, url
 from django.views.generic.simple import direct_to_template
 
 
