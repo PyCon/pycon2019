32a045439dd670b3a2dba9718e0585fd50e2b4b9
Luke Hatcher <lukeman@gmail.com>
2012-08-15 01:16:04 -0400

    add associations

--- symposion/social_auth/urls.py
+++ symposion/social_auth/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from symposion.social_auth.views import SocialAuths
 
 urlpatterns = patterns("",
-    url(r"^associations/$", SocialAuths.as_view(), name="social_auth_associations"),
+    url(r"^$", SocialAuths.as_view(), name="social_auth_associations"),
 )
