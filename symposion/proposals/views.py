5a70473690489e0a0ff39fcf9f14d9cc2a7e8e2a
Luke Hatcher <lukeman@gmail.com>
2012-10-04 19:15:33 -0400

    bug fix additional speaker removal now that we're using a through table

--- symposion/proposals/views.py
+++ symposion/proposals/views.py
@@ -298,7 +298,8 @@ def proposal_leave(request, pk):
     except ObjectDoesNotExist:
         return HttpResponseForbidden()
     if request.method == "POST":
-        proposal.additional_speakers.remove(speaker)
+        speaking = get_object_or_404(AdditionalSpeaker, speaker=request.user.speaker_profile, proposalbase=proposal)
+        speaking.delete()
         # @@@ fire off email to submitter and other speakers
         messages.success(request, "You are no longer speaking on %s" % proposal.title)
         return redirect("dashboard")
