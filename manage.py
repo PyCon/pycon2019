35584bb9f7cc241388793c46ddddc8952d53c89a
Dan Poirier <dpoirier@caktusgroup.com>
2013-06-04 11:24:26 -0400

    Update versions of required software to latest
    Including Django 1.5.1 and some changes to the code required to make it
    work.

--- manage.py
+++ manage.py
@@ -2,8 +2,11 @@
 import os, sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "symposion.settings")
-    
+    if 'test' in sys.argv:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "symposion.settings_test")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "symposion.settings")
+
     from django.core.management import execute_from_command_line
     
     execute_from_command_line(sys.argv)
