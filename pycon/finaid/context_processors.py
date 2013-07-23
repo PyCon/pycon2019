aea7987a6615b5a66de0d09ca28596f42692097b
Dan Poirier <dpoirier@caktusgroup.com>
2013-07-23 08:52:15 -0400

    Simplify deciding which finaid parts to show on dashboard

--- pycon/finaid/context_processors.py
+++ pycon/finaid/context_processors.py
@@ -3,14 +3,16 @@ from pycon.finaid.utils import applications_open, has_application, is_reviewer
 
 def financial_aid(request):
 
+    open = applications_open()
     ctx = {
-        "financial_aid_open": applications_open(),
-        "is_financial_aid_reviewer": is_reviewer(request.user),
-        "has_applied_for_financial_aid": has_application(request.user),
+        "show_finaid_apply_button": open and not has_application(request.user),
+        "show_finaid_edit_button": open and has_application(request.user),
+        "show_finaid_status_button": False,  # FIXME: implement status
+        "show_finaid_review_button": is_reviewer(request.user),
     }
+
     ctx["show_financial_aid_section"] = \
-        ctx['financial_aid_open'] \
-        or ctx['is_financial_aid_reviewer'] \
-        or ctx['has_applied_for_financial_aid']
+        ctx["show_finaid_apply_button"] or ctx["show_finaid_edit_button"] \
+        or ctx["show_finaid_status_button"] or ctx["show_finaid_review_button"]
 
     return ctx
