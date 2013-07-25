c7620faecb609bc7a443e21a414eafbea537177c
Dan Poirier <dpoirier@caktusgroup.com>
2013-07-25 10:57:04 -0400

    Fix hotel dates
    * Hide hotel dates unless hotel grant is checked
    * Give hotel dates default values so that the form will be valid
      if someone never checks hotel grant

--- pycon/static/js/finaid.js
+++ pycon/static/js/finaid.js
@@ -1,7 +1,7 @@
 $(function () {
   /* which controls determine which other controls are visible */
   var controls = {
-    'input#id_hotel_grant_requested': 'div#div_id_hotel_nights,div#div_id_sex',
+    'input#id_hotel_grant_requested': 'div#div_id_hotel_nights,div#div_id_sex,div#div_id_hotel_arrival_date,div#div_id_hotel_departure_date',
     'input#id_travel_grant_requested': 'div#div_id_international,div#div_id_travel_amount_requested,div#div_id_travel_plans'
   };
 
