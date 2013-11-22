7b7fbf04e61dad4180fc383ec63a0d624ba2fbb0
Owen Nelson <onelson@gmail.com>
2013-11-21 20:40:55 -0800

    sup dawg, I heard you needed some debug log level in your prod logs. logs.

--- pycon/settings/production.py
+++ pycon/settings/production.py
@@ -10,3 +10,11 @@ LOGGING['filters']['static_fields']['fields']['environment'] = 'production'
 ALLOWED_HOSTS = [
     'us.pycon.org',
 ]
+
+LOGGING['handlers']['console']['filters'] = []
+LOGGING[''] = {
+    'handlers': ['console'],
+    'level': 'DEBUG',
+}
+import logging
+logging.basicConfig(level=logging.DEBUG)
