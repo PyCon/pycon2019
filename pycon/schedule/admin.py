9768c4b2461ee03790abdd0f38269e600eac481c
Mathieu Leduc-Hamel <marrakis@gmail.com>
2014-02-24 10:43:10 -0500

    Added session role to the admin

--- pycon/schedule/admin.py
+++ pycon/schedule/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from pycon.schedule.models import Session
+from pycon.schedule.models import Session, SessionRole
 
 
 admin.site.register(Session)
+admin.site.register(SessionRole)
