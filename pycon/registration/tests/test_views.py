5c62f888f65575abb5ba9f7f85db34f7fb5e5e26
Rebecca <rebecca@caktusgroup.com>
2014-11-19 09:45:04 -0500

    Move registrations tests to subfolder

--- pycon/registration/tests/test_views.py
+++ pycon/registration/tests/test_views.py
@@ -3,16 +3,16 @@ from django.core.urlresolvers import reverse
 from pycon.tests.base import ViewTestCase
 
 
-class TestRegistration(ViewTestCase):
+class TestCTERegistrationLogin(ViewTestCase):
     url_name = "registration_login"
 
-    def test_cte_login_unauthenticated(self):
+    def test_unauthenticated(self):
         """View requires authentication."""
         self.client.logout()
         response = self.client.get(reverse(self.url_name))
         self.assertRedirectsToLogin(response)
 
-    def test_cte_login_authenticated(self):
+    def test_authenticated(self):
         """View requires authentication."""
         self.login_user()
         response = self.client.get(reverse(self.url_name))
