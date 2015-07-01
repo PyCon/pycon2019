05cf3cc740617929f6567043ac8aef66eea15f2e
Dan Poirier <dpoirier@caktusgroup.com>
2015-07-01 10:07:41 -0400

    Install same version of node and less as our servers

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -7,4 +7,17 @@ hash -r
 pip install -U pip
 hash -r
 pip install virtualenv virtualenvwrapper
-sudo -u postgres createuser --superuser vagrant
+sudo -u postgres createuser --superuser vagrant || true
+
+# We want nodejs 0.10.11 to match our Chef servers
+if [ ! -e /usr/local/bin/npm ] ; then
+  wget https://nodejs.org/dist/v0.10.11/node-v0.10.11-linux-x64.tar.gz
+  tar xzf node-v0.10.11-linux-x64.tar.gz
+  (tar cf - -C node-v0.10.11-linux-x64) | (tar xf - -C /usr/local)
+fi
+
+# And postgres 9.3
+# TODO!
+
+# Now we can install less
+npm install -g less@1.3.3
