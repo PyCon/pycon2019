ea3e2fdcded459b14226f836434c4d4e1508d80f
Dan Poirier <dpoirier@caktusgroup.com>
2015-07-01 08:45:19 -0400

    Install Make

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -2,7 +2,7 @@
 set -e
 apt-get update -qqy
 apt-get purge python-pip -y
-apt-get install python-pip git-core postgresql-9.1 postgresql-client-9.1 postgresql-server-dev-9.1 libpq5 libpq-dev python-dev -y
+apt-get install python-pip git-core postgresql-9.1 postgresql-client-9.1 postgresql-server-dev-9.1 libpq5 libpq-dev python-dev make -y
 hash -r
 pip install -U pip
 hash -r
