ac20ed056dfea123faffb6d9bb67a6dc617c6371
Trevor Ray <tray@caktusgroup.com>
2015-11-03 09:56:40 -0500

    Moved sponsor slider javascript block to site_base.html

---
+++ symposion/conference/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('conference', '0005_auto_20151006_1002'),
+        ('conference', '0005_auto_20150928_1650'),
+    ]
+
+    operations = [
+    ]
