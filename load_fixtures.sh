2d2d4951832ad67384b7381c7a326c944010e3df
Brandon Rhodes <brandon@rhodesmill.org>
2016-01-17 19:12:18 -0500

    Make "load_fixtures.sh" able to run without error
    This is another step towards #579.  I am not sure why we have fixtures
    that error out trying to create database rows that already exist, but
    this disables them so that a basic site can now come up and running with
    some content.

--- load_fixtures.sh
+++ load_fixtures.sh
@@ -8,17 +8,22 @@ python manage.py loaddata \
   fixtures/initial_data.json \
   fixtures/pycon.json \
   fixtures/cms_initial_pages.json \
-  fixtures/conference.json \
   fixtures/initial_boxes.json \
   fixtures/initial_data.json \
-  fixtures/proposal_base.json \
   fixtures/sitetree_menu.json \
   fixtures/sponsorship_benefits.json \
   fixtures/sponsorship_levels.json \
-  fixtures/tutorials_schedule.json \
-  fixtures/talks_schedule.json \
-  fixtures/permissions.json \
-  fixtures/teams.json \
+
+# The following fixtures used to be in the above list, but now seem to
+# collide with IDs and objects that are created during the initial
+# "migrate" that creates the Django database:
+#
+#  fixtures/conference.json \
+#  fixtures/proposal_base.json \
+#  fixtures/tutorials_schedule.json \
+#  fixtures/talks_schedule.json \
+#  fixtures/permissions.json \
+#  fixtures/teams.json \
 
 echo
 echo
