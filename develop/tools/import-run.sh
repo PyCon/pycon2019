9090b19f45624f554b1aebc6ef811817bc54348d
Brandon Rhodes <brandon@rhodesmill.org>
2016-03-05 16:33:32 -0500

    Add plenary sessions and breaks to imports

--- develop/tools/import-run.sh
+++ develop/tools/import-run.sh
@@ -5,51 +5,55 @@
 set -e
 
 cat > breaks.csv <<EOF
-kind_slug,kind_label,day,start,duration
-sponsor-tutorial,Break,2016-05-28,10:30,30
-sponsor-tutorial,Lunch,2016-05-28,12:30,60
-sponsor-tutorial,Lunch,2016-05-29,12:30,60
-sponsor-tutorial,Lunch,2016-05-29,12:30,60
-tutorial,Lunch,2016-05-28,12:20,60
-tutorial,Lunch,2016-05-28,12:20,60
-tutorial,Lunch,2016-05-28,12:20,60
-tutorial,Lunch,2016-05-28,12:20,60
-tutorial,Lunch,2016-05-28,12:20,60
-tutorial,Lunch,2016-05-28,12:20,60
-tutorial,Lunch,2016-05-28,12:20,60
-tutorial,Lunch,2016-05-28,12:20,60
-tutorial,Lunch,2016-05-28,12:20,60
-tutorial,Lunch,2016-05-29,12:20,60
-tutorial,Lunch,2016-05-29,12:20,60
-tutorial,Lunch,2016-05-29,12:20,60
-tutorial,Lunch,2016-05-29,12:20,60
-tutorial,Lunch,2016-05-29,12:20,60
-tutorial,Lunch,2016-05-29,12:20,60
-tutorial,Lunch,2016-05-29,12:20,60
-tutorial,Lunch,2016-05-29,12:20,60
-tutorial,Lunch,2016-05-29,12:20,60
-talk,Breakfast,2016-05-30,8:00,60
-talk,Break,2016-05-30,10:10,40
-talk,Lunch,2016-05-30,12:40,60
-talk,Lunch,2016-05-30,12:40,60
-talk,Lunch,2016-05-30,12:55,60
-talk,Lunch,2016-05-30,12:55,60
-talk,Lunch,2016-05-30,12:55,60
-talk,Lunch,2016-05-31,12:40,60
-talk,Lunch,2016-05-31,12:40,60
-talk,Lunch,2016-05-31,12:55,60
-talk,Lunch,2016-05-31,12:55,60
-talk,Lunch,2016-05-31,12:55,60
-talk,Break,2016-05-30,15:45,30
-talk,Break,2016-05-30,15:45,30
-talk,Break,2016-05-30,16:00,30
-talk,Break,2016-05-30,16:00,30
-talk,Break,2016-05-30,16:00,30
-talk,Break,2016-05-31,15:45,30
-talk,Break,2016-05-31,15:45,30
-talk,Break,2016-05-31,16:00,30
-talk,Break,2016-05-31,16:00,30
-talk,Break,2016-05-31,16:00,30
+kind_slug,kind_label,day,start,duration,content_override
+sponsor-tutorial,Break,2016-05-28,10:30,30,Sponsor Room 1,
+sponsor-tutorial,Lunch,2016-05-28,12:30,60,Sponsor Room 2,
+sponsor-tutorial,Lunch,2016-05-29,12:30,60,Sponsor Room 1,
+sponsor-tutorial,Lunch,2016-05-29,12:30,60,Sponsor Room 2,
+tutorial,Lunch,2016-05-28,12:20,60,Tutorial %,
+tutorial,Lunch,2016-05-29,12:20,60,Tutorial %,
+tutorial, ,2016-05-29,16:40,80,Tutorial %,
+tutorial,plenary,2016-05-29,18:00,180,Tutorial %,Opening Reception
+talk,Breakfast,2016-05-30,8:00,60,Session %,
+talk,plenary,2016-05-30,9:00,30,Session %,Welcome to PyCon
+talk,plenary,2016-05-30,9:30,40,Session %,Keynote — Lorena Barba
+talk,Break,2016-05-30,10:10,40,Session %,
+talk,Lunch,2016-05-30,12:40,60,Session A,
+talk,Lunch,2016-05-30,12:40,60,Session B,
+talk,Lunch,2016-05-30,12:55,60,Session C,
+talk,Lunch,2016-05-30,12:55,60,Session D,
+talk,Lunch,2016-05-30,12:55,60,Session E,
+talk,Break,2016-05-30,15:45,30,Session A,
+talk,Break,2016-05-30,15:45,30,Session B,
+talk,Break,2016-05-30,16:00,30,Session C,
+talk,Break,2016-05-30,16:00,30,Session D,
+talk,Break,2016-05-30,16:00,30,Session E,
+talk,plenary,2016-05-30,17:40,60,Session %,Lightning Talks
+talk,Breakfast,2016-05-31,8:00,30,Session %,
+talk,plenary,2016-05-31,8:30,30,Session %,Lightning Talks
+talk,plenary,2016-05-31,9:00,40,Session %,Python Language — Guido van Rossum
+talk,plenary,2016-05-31,9:40,40,Session %,Keynote — Parisa Tabriz
+talk,Break,2016-05-31,10:20,30,Session %,
+talk,Lunch,2016-05-31,12:40,60,Session A,
+talk,Lunch,2016-05-31,12:40,60,Session B,
+talk,Lunch,2016-05-31,12:55,60,Session C,
+talk,Lunch,2016-05-31,12:55,60,Session D,
+talk,Lunch,2016-05-31,12:55,60,Session E,
+talk,Break,2016-05-31,15:45,30,Session A,
+talk,Break,2016-05-31,15:45,30,Session B,
+talk,Break,2016-05-31,16:00,30,Session C,
+talk,Break,2016-05-31,16:00,30,Session D,
+talk,Break,2016-05-31,16:00,30,Session E,
+talk,plenary,2016-05-31,17:40,60,Session %,Lightning Talks
+talk,Breakfast,2016-06-01,8:00,30,Session %,
+talk,plenary,2016-06-01,8:30,30,Session %,Lightning Talks
+talk,plenary,2016-06-01,9:00,20,Session %,Python Software Foundation — Van Lindberg
+talk,plenary,2016-06-01,9:20,40,Session %,Keynote — Cris Ewing
+talk,Break,2016-06-01,10:00,190,Session %,Poster Session / Job Fair / Lunch — Expo Hall
+talk,plenary,2016-06-01,15:10,40,Session %,Keynote — K Lars Lohn
+talk,plenary,2016-06-01,15:50,20,Session %,Closing Remarks
+talk, ,2016-06-01,16:10,20,Session %,
+talk,Introduction to Sprints,2016-06-01,16:30,90,Session %,
 EOF
 
 psql "${1:-pycon2016}" <<'EOF'
@@ -57,11 +61,14 @@ psql "${1:-pycon2016}" <<'EOF'
 begin;
 
 create temporary table b (
+ slot_id serial,
  kind_slug text,
  kind_label text,
  day date,
  start time,
- duration integer
+ duration integer,
+ room_name text,
+ content_override text
 );
 
 create temporary table s (
@@ -73,7 +80,7 @@ create temporary table s (
  room_name text
 );
 
-\copy b from 'breaks.csv' csv header;
+\copy b (kind_slug, kind_label, day, start, duration, room_name, content_override) from 'breaks.csv' csv header;
 \copy s from 'schedule.csv' csv header;
 
 alter table b add column schedule_id integer;
@@ -133,11 +140,12 @@ insert into symposion_schedule_room (name, "order", schedule_id)
 update symposion_schedule_room set "order" = id;
 
 insert into symposion_schedule_slot
+ (id, start, "end", content_override, day_id, kind_id)
  select
-  10 + row_number() over (order by start),
+  b.slot_id,
   start,
   start + cast(duration || ' minutes' as interval),
-  '',
+  coalesce(b.content_override, ''),
   (select id from symposion_schedule_day ssd
     where ssd.date = day and ssd.schedule_id = b.schedule_id),
   (select id from symposion_schedule_slotkind ssk
@@ -145,6 +153,7 @@ insert into symposion_schedule_slot
  from b;
 
 insert into symposion_schedule_slot
+ (id, start, "end", content_override, day_id, kind_id)
  select
   proposal_id,
   start,
@@ -155,9 +164,15 @@ insert into symposion_schedule_slot
   (select id from symposion_schedule_slotkind where label = kind_slug)
  from s;
 
-insert into symposion_schedule_slotroom
+insert into symposion_schedule_slotroom (room_id, slot_id)
  select
-  proposal_id,
+  ssr.id,
+  b.slot_id
+ from b
+  join symposion_schedule_room ssr on (ssr.name like b.room_name);
+
+insert into symposion_schedule_slotroom (room_id, slot_id)
+ select
   (select id from symposion_schedule_room where name = room_name),
   proposal_id
  from s;
