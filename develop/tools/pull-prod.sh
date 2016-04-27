fc2b212f328e6a8290d7dcc066a983212d3d01e1
Brandon Rhodes <brandon@rhodesmill.org>
2016-04-27 07:45:11 -0400

    Rename dev database when pulling it from prod

--- develop/tools/pull-prod.sh
+++ develop/tools/pull-prod.sh
@@ -5,3 +5,6 @@
 
 ssh pycon pg_dump -Fc '"$(cat dsn)"' |
     vagrant ssh -- pg_restore -d template1 --create --clean
+
+echo 'alter database "pycon-prod" rename to pycon2016;' |
+    vagrant ssh -- psql template1
