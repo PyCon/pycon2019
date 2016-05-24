2b6ea018a406f4213eda22403525632aa499e8c6
David Ray <dray@caktusgroup.com>
2016-05-24 08:17:12 -0400

    Adds an admin interface for Financial Aid Receipts

--- pycon/finaid/admin.py
+++ pycon/finaid/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
 from .models import FinancialAidApplication, FinancialAidApplicationPeriod,\
-    FinancialAidMessage, FinancialAidEmailTemplate
+    FinancialAidMessage, FinancialAidEmailTemplate, Receipt
 
 
-def application__user(message):
-    return message.application.user
+def application__user(obj):
+    return obj.application.user
 application__user.short_description = u"Applicant"
 
 
@@ -13,7 +13,13 @@ class MessageAdmin(admin.ModelAdmin):
     list_display = ('submitted_at', 'user', application__user)
 
 
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ('timestamp', application__user, 'amount', 'receipt_image', )
+    list_filter = ('application__user', )
+
+
 admin.site.register(FinancialAidApplication)
 admin.site.register(FinancialAidApplicationPeriod)
 admin.site.register(FinancialAidMessage, MessageAdmin)
 admin.site.register(FinancialAidEmailTemplate)
+admin.site.register(Receipt, ReceiptAdmin)
