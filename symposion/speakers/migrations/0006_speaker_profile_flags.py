cf9bd4cc9483b16187fc19e2935e81ef8577bf54
Ernest W. Durbin III <ewdurbin@gmail.com>
2017-09-24 11:09:39 -0400

    help_text only in form, lets us change w/o migrations

--- symposion/speakers/migrations/0006_speaker_profile_flags.py
+++ symposion/speakers/migrations/0006_speaker_profile_flags.py
@@ -15,16 +15,16 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='speaker',
             name='financial_support',
-            field=models.BooleanField(default=False, help_text='PyCon does not want expenses to discourage you from submitting a proposal, and offers financial support with a preference for speakers. Check here to indicate that you require assistance. This is not seen by the proposal reviewers and does not affect the review of  your proposal.'),
+            field=models.BooleanField(default=False),
         ),
         migrations.AddField(
             model_name='speaker',
             name='interested_mentee',
-            field=multiselectfield.db.fields.MultiSelectField(help_text="I'm interested in receiving mentorship from others for my proposals in the following ways:", max_length=47, choices=[(b'brainstorming', b'Brainstorming'), (b'proposal_creation', b'Proposal Creation'), (b'proposal_review', b'Proposal Review')]),
+            field=multiselectfield.db.fields.MultiSelectField(max_length=47, choices=[(b'brainstorming', b'Brainstorming'), (b'proposal_creation', b'Proposal Creation'), (b'proposal_review', b'Proposal Review')]),
         ),
         migrations.AddField(
             model_name='speaker',
             name='interested_mentor',
-            field=multiselectfield.db.fields.MultiSelectField(help_text="I'm interested in providing mentorship for others submitting proposals in the following ways:", max_length=47, choices=[(b'brainstorming', b'Brainstorming'), (b'proposal_creation', b'Proposal Creation'), (b'proposal_review', b'Proposal Review')]),
+            field=multiselectfield.db.fields.MultiSelectField(max_length=47, choices=[(b'brainstorming', b'Brainstorming'), (b'proposal_creation', b'Proposal Creation'), (b'proposal_review', b'Proposal Review')]),
         ),
     ]
