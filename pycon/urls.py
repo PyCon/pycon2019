9dbf75f54949286eb4f281da664e6bd1e29ab5ff
Ernest W Durbin III <ewdurbin@gmail.com>
2018-03-13 20:02:32 -0400

    tweaks for recent shipments

--- pycon/urls.py
+++ pycon/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = patterns(
     '',
     url('program_export/', views.program_export, name='program_export'),
-    url(r'^event/overview/$', views.scheduled_event_overview, name='scheduled_event_overview'),
+    url(r'^events/overview/$', views.scheduled_event_overview, name='scheduled_event_overview'),
     url(r'^event/(?P<slug>.*)/$', views.scheduled_event, name='scheduled_event'),
 )
