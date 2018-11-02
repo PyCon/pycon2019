d5f9a558cd874dd1d757fd128b4c607379280c8a
Ernest W Durbin III <ewdurbin@gmail.com>
2018-11-02 08:48:49 -0400

    update help texts on Talk proposals

--- pycon/forms.py
+++ pycon/forms.py
@@ -67,6 +67,92 @@ class PyConTalkProposalForm(PyConProposalForm):
                 Puns, jokes, or “hooks” in titles are okay,
                 but make sure that if all someone knew was the title,
                 they still would have some idea what the presentation is about.
+                <br><br>
+                <b>Please do not include any personally identifiable information.</b>
+                The initial round of reviews are annonymous, and this field will
+                visible to reviewers.<br>
+                """
+            ),
+            'description': strip(
+                u"""
+                Both your title and this description are made public
+                and displayed in the conference program
+                to help attendees decide
+                whether they are interested in this presentation.
+                Limit this description to a few concise paragraphs.
+                <br><br>
+                <b>Please do not include any personally identifiable information.</b>
+                The initial round of reviews are annonymous, and this field will
+                visible to reviewers.<br>
+                """
+            ),
+            'additional_notes': strip(
+                u"""
+                Anything else you would like to share with the committee:<br>
+                Speaker public speaking experience.<br>
+                Speaker subject matter experience.<br>
+                Have the speaker(s) given this presentation before elsewhere?<br>
+                Links to recordings, slides, blog posts, code, or other material.
+                <br>
+                Specific needs or special requests — accessibility,
+                audio (will you need to play pre-recorded sound?),
+                or restrictions on when your talk can be scheduled.
+                <br>
+                This field will be visible to reviewers in the second round of
+                proposal review.
+                """,
+            ),
+            'outline': strip(
+                u"""
+                The “outline” is a skeleton of your talk that is as detailed as
+                possible, including rough timings or estimates for different
+                sections. If requesting a 45 minute slot, please describe what
+                content would appear in the 45 minute version but not a 30 minute
+                version, either within the outline or in a paragraph at the
+                end.
+                <br><br>
+                <b>Please do not include any personally identifiable information.</b>
+                The initial round of reviews are annonymous, and this field will
+                visible to reviewers.<br>
+                <br>
+                <i>Committee note:</i> The outline is extremely important for the
+                program committee to understand what the content and structure of
+                your talk will be. The timings/percentages help us compare multiple
+                talks that might have a similar abstract. We know that they are
+                estimates and only capture your view at this moment in time and are
+                likely to change before PyCon. We hope that writing the outline is
+                helpful to you as well, to organize and clarify your thoughts for
+                your talk! The outline will not be shared with conference
+                attendees.<br>
+                <br>
+                If there’s too much to your topic to cover even in 45 minutes, you
+                may wish to narrow it down. Alternatively, consider submitting a
+                3-hour PyCon tutorial instead. If you plan to do live coding during
+                your talk, please describe your backup plan in case the live coding
+                fails (for whatever reason). Suggestions include a pre-recorded
+                video, or slides to replace the live coding.
+                """
+            ),
+            'audience': strip(
+                u"""
+                1–2 paragraphs that should answer three questions:
+                (1) Who is this talk for?
+                (2) What background knowledge or experience
+                do you expect the audience to have?
+                (3) What do you expect the audience to learn or do
+                after watching the talk?
+                <br><br>
+                <b>Please do not include any personally identifiable information.</b>
+                The initial round of reviews are annonymous, and this field will
+                visible to reviewers.<br><br>
+                <i>Committee note:</i> The “Audience” section
+                helps the program committee get a sense
+                of whether your talk is geared more at novices
+                or experienced individuals in a given subject.
+                (We need a balance of both lower-level and advanced talks
+                to make a great PyCon!)
+                It also helps us evaluate the relevance of your talk
+                to the Python community.
                 """
             ),
         }
