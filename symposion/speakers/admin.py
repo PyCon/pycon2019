3a64a91a82afd9f434d88504abe1446ffb86998c
Ernest W. Durbin III <ewdurbin@gmail.com>
2019-02-06 11:25:07 -0500

    Update admin.py

--- symposion/speakers/admin.py
+++ symposion/speakers/admin.py
@@ -9,7 +9,7 @@ class SpeakerAdmin(MarkEditAdmin):
     list_display = ["name", "email", "created", "twitter_username",
                     "mobile_number"]
     raw_id_fields = ["user"]
-    search_fields = ["name", "twitter_username"]
+    search_fields = ["name", "email", "twitter_username"]
 
     class MarkEdit:
         fields = []
