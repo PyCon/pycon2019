7d34c1736dfef09a62860dcc689da48e4e0c2444
Ernest W. Durbin III <ewdurbin@gmail.com>
2019-03-21 09:44:23 -0500

    if registration vendor returns a different email address, associate it with the user

--- pycon/tests/test_commands.py
+++ pycon/tests/test_commands.py
@@ -4,6 +4,8 @@ from mock import Mock, patch
 
 from requests.exceptions import HTTPError
 
+from account.models import EmailAddress
+
 from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.test import TestCase
@@ -111,3 +113,43 @@ class UpdateTutorialRegistrantsTestCase(TestCase):
         # updated; dropping u2 and adding u1
         self.assertEqual(1, tut2.registrants.all().count())
         self.assertIn(u1, tut2.registrants.all())
+
+    def test_surprise_email_address(self, mock_get):
+        """Simple Test Case where registrant has a different email than our DB"""
+        user_model = get_user_model()
+        u1 = user_model.objects.create_user('john', email='John@doe.com', password='1234')
+        u2 = user_model.objects.create_user('jane', email='jane.doe@gmail.com', password='1234')
+
+        tut1 = PyConTutorialProposalFactory(title='Tutorial1')
+        PresentationFactory(proposal_base=tut1)
+        tut2 = PyConTutorialProposalFactory(title='Tutorial2')
+        PresentationFactory(proposal_base=tut2)
+
+        # Add u2 to tut2
+        tut2.registrants.add(u2)
+        self.assertIsNone(tut1.max_attendees)
+        self.assertIn(u2, tut2.registrants.all())
+
+        mock_get.return_value = MockGet(tut1=tut1.pk,
+                                        tut2=tut2.pk,
+                                        u1=u1.pk,
+                                        u2=u2.pk)
+        call_command('update_tutorial_registrants')
+
+        tut1 = PyConTutorialProposal.objects.get(pk=tut1.pk)
+        self.assertEqual(8, tut1.max_attendees)
+        self.assertEqual(2, tut1.registrants.all().count())
+        for u in [u1, u2]:
+            self.assertIn(u, tut1.registrants.all())
+
+        tut2 = PyConTutorialProposal.objects.get(pk=tut2.pk)
+        self.assertEqual(10, tut2.max_attendees)
+        # updated; dropping u2 and adding u1
+        self.assertEqual(1, tut2.registrants.all().count())
+        self.assertIn(u1, tut2.registrants.all())
+
+        emails1 = EmailAddress.objects.filter(user=u1)
+        emails2 = EmailAddress.objects.filter(user=u2)
+
+        self.assertEqual(['John@doe.com'], [e.email for e in emails1])
+        self.assertEqual(['jane@doe.com', 'jane.doe@gmail.com'], [e.email for e in emails2])
