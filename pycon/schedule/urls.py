f8ee4b31328b6101ff0a461330348692bbcc08b0
Brian Costlow <brian.costlow@gmail.com>
2019-04-22 15:57:41 -0400

    Url for slides download page.

--- pycon/schedule/urls.py
+++ pycon/schedule/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns(
     url(r"^session/(\d+)/$", "session_detail", name="schedule_session_detail"),
     url(r"^session-staff.json", "session_staff_json", name="session_staff_json"),
     url(r"^slides_upload/(?P<presentation_id>[0-9,]+)/$", "slides_upload", name="pycon_schedule_slides_upload"),
+    url(r"^slides_download/$", "slides_download", name="pycon_schedule_slides_download"),
 )
